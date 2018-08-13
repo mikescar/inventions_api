4b3a874b47b25d62cf2e662205cbf6d2cd9b662a
mikescar <mike@tenthpeak.us>
2018-08-13 07:31:46 -0600

    Move docker build/push to its own script

---
+++ bin/docker-deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -eo pipefail
+
+TAG="registry.heroku.com/${APP_NAME_PRODUCTION}/web"
+
+docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
+docker build --rm=false -t $TAG .
+
+IMAGE_ID=$(docker inspect $TAG --format={{.Id}} | cut -d':' -f2)
+echo "Image ID is: $IMAGE_ID"
+
+docker push $TAG
+
+# TODO exit 1 if curl command returns error message
+curl -n -X PATCH https://api.heroku.com/apps/${APP_NAME_PRODUCTION}/formation \
+  -d '{
+  "updates": [
+    {
+      "type": "web",
+      "docker_image": "$IMAGE_ID"
+    }
+  ]
+}' \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+  -H "Authorization: Bearer $HEROKU_API_KEY"
