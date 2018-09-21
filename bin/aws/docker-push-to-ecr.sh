8827a11cfb765ad7275ba7619ec5f479f97ed5d1
mikescar <mike@tenthpeak.us>
2018-09-20 23:04:26 -0600

    Fix tagging, aws docs wrong

--- bin/aws/docker-push-to-ecr.sh
+++ bin/aws/docker-push-to-ecr.sh
@@ -7,12 +7,12 @@ pip install awscli
 
 $(aws ecr get-login --no-include-email --region $AWS_REGION)
 
-docker build --rm=false -t $AWS_ECR_TAG .
+VERSIONED_TAG="${AWS_ECR_REPO_NAME}:${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}"
+docker build --rm=false -t $VERSIONED_TAG .
 
-IMAGE_ID=$(docker inspect $AWS_ECR_TAG --format={{.Id}} | cut -d':' -f2)
+IMAGE_ID=$(docker inspect $VERSIONED_TAG --format={{.Id}} | cut -d':' -f2)
 echo "Image ID is: $IMAGE_ID"
 
-VERSIONED_TAG="${AWS_ECR_TAG}:${CIRCLE_BRANCH}-${CIRCLE_BUILD_NUM}"
 TARGET_TAG="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${VERSIONED_TAG}"
 docker tag $VERSIONED_TAG $TARGET_TAG
 docker push $TARGET_TAG
