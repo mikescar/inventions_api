da5916b1d8634c30629ddfd9d647f1542171adae
mikescar <mike@tenthpeak.us>
2018-09-23 09:54:07 -0600

    Split out heroku deploy request, debug

--- bin/heroku/docker-push.sh
+++ bin/heroku/docker-push.sh
@@ -13,16 +13,27 @@ echo Image ID is: $IMAGE_ID
 docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
 docker push $TARGET_IMAGE
 
-# TODO exit 1 if curl exits 0 but outputs error message
-curl -n -X PATCH https://api.heroku.com/apps/${APP_NAME_PRODUCTION}/formation \
-  -d "{
-    \"updates\": [
+sleep 5s
+
+generate_patch_data()
+{
+  cat <<EOF
+  {
+    "updates": [
       {
-        \"type\": \"web\",
-        \"docker_image\": \"$IMAGE_ID\"
+        "type": "web",
+        "docker_image": "$IMAGE_ID"
       }
     ]
-  }" \
+  }
+EOF
+}
+
+echo Requesting image deploy for: $(generate_patch_data)
+
+# TODO exit 1 if curl exits 0 but outputs error message
+curl -n -X PATCH https://api.heroku.com/apps/${APP_NAME_PRODUCTION}/formation \
+  --data "$(generate_patch_data)" \
   -H "Content-Type: application/json" \
   -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
   -H "Authorization: Bearer $HEROKU_API_KEY"
