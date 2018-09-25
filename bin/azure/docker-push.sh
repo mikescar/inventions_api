301678a187aba1413b91bccb11f3f0fc9b7e5f4b
mikescar <mike@tenthpeak.us>
2018-09-24 20:06:03 -0600

    Add comments for Azure CD

--- bin/azure/docker-push.sh
+++ bin/azure/docker-push.sh
@@ -7,12 +7,15 @@ TARGET_IMAGE="${CR_DOMAIN}/${APP_NAME_PRODUCTION}:${SOURCE_IMAGE}"
 
 docker tag $SOURCE_IMAGE $TARGET_IMAGE
 
-echo Image ID is: $(docker inspect $TARGET_IMAGE --format={{.Id}} | cut -d':' -f2)
+IMAGE_ID=$(docker inspect $TARGET_IMAGE --format={{.Id}} | cut -d':' -f2)
+echo Image ID is: $IMAGE_ID
 
+# Examples for creating and updating roles for Azure service principals:
+# https://github.com/Azure-Samples/azure-cli-samples/tree/master/container-registry
 docker login --username=$AZURE_SP_ID --password=$AZURE_SP_PASSWD $CR_DOMAIN
 docker push $TARGET_IMAGE
 
-# Actually deploy in ACI
+# Deploy image to ACI
 # az container create \
 #     --resource-group demos \
 #     --name $APP_NAME_PRODUCTION \
